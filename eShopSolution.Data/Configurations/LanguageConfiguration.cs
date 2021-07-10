9f006ab4b184d521b4e316b53195cb69abecbd19
nguyetpham09 <nguyetpham969@gmail.com>
2021-07-10 15:56:40 +0700

    migration database

--- eShopSolution.Data/Configurations/LanguageConfiguration.cs
+++ eShopSolution.Data/Configurations/LanguageConfiguration.cs
@@ -15,7 +15,7 @@ namespace eShopSolution.Data.Configurations
 
             builder.HasKey(x => x.Id);
 
-            builder.Property(x => x.Id).IsRequired().HasMaxLength(5);
+            builder.Property(x => x.Id).IsUnicode(false).HasMaxLength(5);
 
             builder.Property(x => x.Name).IsRequired().HasMaxLength(20);
         }
