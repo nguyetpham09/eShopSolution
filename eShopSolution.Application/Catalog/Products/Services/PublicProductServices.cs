45eb8fdd58ef88b24e13c67d0fe741039afcd484
nguyetpham09 <nguyetpham969@gmail.com>
2021-07-24 10:58:25 +0700

    add swagger

--- eShopSolution.Application/Catalog/Products/Services/PublicProductServices.cs
+++ eShopSolution.Application/Catalog/Products/Services/PublicProductServices.cs
@@ -17,6 +17,34 @@ namespace eShopSolution.Application.Catalog.Products.Services
         {
             _context = context;
         }
+
+        public async Task<List<ProductViewModel>> GetAll()
+        {
+            var query = from p in _context.Products
+                        join pt in _context.ProductTranslations on p.Id equals pt.ProductId
+                        join pic in _context.ProductInCategories on p.Id equals pic.ProductId
+                        join c in _context.Categories on pic.CategoryId equals c.Id
+                        select new { p, pt, pic };
+            var data = await query.Select(x => new ProductViewModel()
+            {
+                Id = x.p.Id,
+                DateCreated = x.p.DateCreated,
+                Description = x.pt.Description,
+                Details = x.pt.Details,
+                LanguageId = x.pt.LanguageId,
+                Name = x.pt.Name,
+                OriginalPrice = x.p.OriginalPrice,
+                Price = x.p.Price,
+                SeoAlias = x.pt.SeoAlias,
+                SeoDescription = x.pt.SeoDescription,
+                SeoTitle = x.pt.SeoTitle,
+                Stock = x.p.Stock,
+                ViewCount = x.p.ViewCount
+            }).ToListAsync();
+            
+            return data;
+        }
+
         public async Task<PageResult<ProductViewModel>> GetAllByCategoryId(GetPublicProductPagingRequest request)
         {
             //Select
