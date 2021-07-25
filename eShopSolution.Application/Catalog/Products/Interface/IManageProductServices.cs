21821ae7caf84efba3fe63e1023d630d935e0062
nguyetpham09 <nguyetpham969@gmail.com>
2021-07-25 15:51:11 +0700

    api for images of product

--- eShopSolution.Application/Catalog/Products/Interface/IManageProductServices.cs
+++ eShopSolution.Application/Catalog/Products/Interface/IManageProductServices.cs
@@ -1,4 +1,5 @@
-﻿using eShopSolution.ViewModels.Catalog.Products;
+﻿using eShopSolution.ViewModels.Catalog.ProductImages;
+using eShopSolution.ViewModels.Catalog.Products;
 using eShopSolution.ViewModels.Common;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
@@ -14,6 +15,8 @@ namespace eShopSolution.Application.Catalog.Products.Interface
         
         Task<int> Delete(int productId);
 
+        Task<ProductViewModel> GetById(int productId, string languageId);
+
         Task<bool> UpdatePrice(int productId, decimal newPrice);
 
         Task<bool> UpdateStock(int productId, int addedQuantity);
@@ -22,12 +25,14 @@ namespace eShopSolution.Application.Catalog.Products.Interface
 
         Task<PageResult<ProductViewModel>> GetAllPaging(GetManageProductPagingRequest request);
 
-        Task<int> AddImages(int productId, List<IFormFile> images);
+        Task<int> AddImage(int productId, ProductImageCreateRequest request);
 
         Task<int> RemoveImage(int imageId);
 
-        Task<int> UpdateImage(int imageId, string caption, bool isDefault);
+        Task<int> UpdateImage(int imageId, ProductImageUpdateRequest request);
 
         Task<List<ProductImageViewModel>> GetProductImages(int productId);
+
+        Task<ProductImageViewModel> GetImageById(int imageId);
     }
 }
